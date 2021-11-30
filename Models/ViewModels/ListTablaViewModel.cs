0556dd19ead17db6f2c210573c1815e9600e0b7f
MichaelLJp <73371194+MichaelLJp@users.noreply.github.com>
2021-11-30 01:57:40 -0500

    Add files via upload

---
+++ Models/ViewModels/ListTablaViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebPrueba1.Models.ViewModels
+{
+    public class ListTablaViewModel
+    {
+        public int id { get; set; }
+        public string nameClient { get; set; }
+        public string documentType { get; set; }
+        public string documentNumber { get; set; }
+        public string addressPlace { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string dateCreate { get; set; }
+
+    }
+}
---
+++ Models/ViewModels/TablaViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebPrueba1.Models.ViewModels
+{
+
+    public class typeDocument
+    {
+        public int typeDocumentId { get; set; }
+        public string Name { get; set; }
+    }
+    public class TablaViewModel
+    {
+
+        public int id { get; set; }
+
+        [Required]
+        [Display(Name ="Nombre")]
+        public string nameClient { get; set; }
+
+        [Required]
+        [Display(Name = "Tipo de documento")]
+        public string documentType { get; set; }
+
+        [Required]
+        [Display(Name = "Número de documento")]
+        public string documentNumber { get; set; }
+
+        [Display(Name = "Dirección")]
+        public string addressPlace { get; set; }
+
+        [Required]
+        [Display(Name = "Teléfono")]
+        public string phone { get; set; }
+
+        [EmailAddress]
+        [Display(Name = "Correo")]
+        public string email { get; set; }
+
+
+        [Display(Name = "Fecha de creación")]
+        public string dateCreate { get; set; }
+
+    }
+}
